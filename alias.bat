f108eb4f2e25da56cb0dd50d00273eba69645cb8
NaveenKumar Namachivayam ⚡ <catch.nkn@gmail.com>
2021-08-16 12:35:40 -0400

    Create alias.bat

---
+++ alias.bat
@@ -0,0 +1,17 @@
+@echo off
+
+DOSKEY jmeter=cd "C:\Training\apache-jmeter-5.4.1\bin"
+DOSKEY camtasia=cd "C:\Users\Navee\OneDrive\Documents\Camtasia"
+DOSKEY tools=cd "C:\Tools"
+DOSKEY gits=cd "C:\Gits"
+
+
+DOSKEY maker=wmic csproduct get vendor
+DOSKEY model=wmic csproduct get name
+DOSKEY patches=wmic qfe list full /format:htable
+DOSKEY bios=WMIC BIOS Get Manufacturer,Name,Version /Format:csv
+
+
+DOSKEY ls=dir /D $*
+DOSKEY ll=dir /A $*
+DOSKEY cat=type $*
